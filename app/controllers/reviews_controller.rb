bbbc9f38ada22ef2fdf43e6e53bb6283086fbeba
Tomoya Fukuda <2311140073n@ed.fuk.kindai.ac.jp>
2025-11-19 08:39:20 +0000

    投稿内容表示

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,6 @@
 class ReviewsController < ApplicationController
   def index
+     @reviews = Review.all
   end
   
   def new
@@ -8,8 +9,10 @@ class ReviewsController < ApplicationController
   
   def create
      
-  bookmark = Review.new(title: params[:review][:storename], url: params[:review][:location])
-  bookmark.save
+  review = Review.new(storename: params[:review][:storename], location: params[:review][:location], comment: params[:review][:comment],
+    category: params[:review][:category],
+    star: params[:review][:star])
+  review.save
   redirect_to '/' #=>一覧ページにリダイレクトする
 end
 end
